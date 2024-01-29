0a2c923c5f0626719470986ab021e43b43d1ba39
Shadowing122 <Addamshover1@gmail.com>
2024-01-29 11:29:19 -0500

    Update ParkingTester.java

--- ParkingTester.java
+++ ParkingTester.java
@@ -1,3 +1,13 @@
 public class ParkingTester {
-    
+    public static void main(String []args){
+        ParkingLot lot = new ParkingLot();
+        System.out.println(lot);
+        Car car1 = new Car("A", "Infiniti", true);
+        int i = lot.parkCar(car1);
+        System.out.println(lot);
+        Car car2 = new Car("B", "Cadiliac", false);
+        int i2 = lot.parkCar(car2);
+        lot.removeCar(i);
+        System.out.println(lot);
+    }
 }
