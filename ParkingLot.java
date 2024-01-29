0eb52f532c1b61934c408c3bb8e1e893aaf7d5e9
Shadowing122 <Addamshover1@gmail.com>
2024-01-29 11:08:36 -0500

    Update ParkingLot.java

--- ParkingLot.java
+++ ParkingLot.java
@@ -16,12 +16,14 @@ public class ParkingLot {
     public int parkCar(Car car){
         int index = -1; // -1 is bad value
         for (int i=0; i<10; i++){
-            if ((this.lot[i].handicap == true) & (car.handicap == true)){
-                index = i;
-                lot[i].car = car;
-            } else if ((this.lot[i].handicap == false) & (car.handicap == false)){
-                index = i;
-                lot[i].car = car;
+            if (this.lot[i].car == null){
+                if ((this.lot[i].handicap == true) &&  (car.handicap == true)){
+                    index = i;
+                    lot[i].car = car;
+                } else if ((this.lot[i].handicap == false) &&  (car.handicap == false)){
+                    index = i;
+                    lot[i].car = car;
+                }
             }
         }
         return index;
@@ -40,11 +42,13 @@ public class ParkingLot {
         int handicapNum = 0;
         int emptyNum = 0;
         for (int i=0; i<this.lot.length; i++){
-            if ((this.lot[i].handicap == true) & (this.lot[i].car == null)){
+            if ((this.lot[i].handicap == true) && (this.lot[i].car == null)){
                 handicapNum += 1;
                 emptyNum += 1;
+            } else if (this.lot[i].car == null){
+                emptyNum += 1;
             }
-        }
-        return "";
+        } 
+        return "" + handicapNum + " " + emptyNum;
     }
 }
